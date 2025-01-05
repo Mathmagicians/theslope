990f0bbb708e70fe7676c859233da272b2e279e9
themathmagician <agata@mathmagicians.dk>
2025-01-05 09:41:57 +0100

    html skeleton mockup

---
+++ theslope/components/Header.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  const items=ref([
+    {name: "Fællesspisning' ',  path: "dinner"}
+  ])
+</script>
+
+<template>
+  <nav class="bg-gray-800 p-4">
+
+  </nav>
+</template>
---
+++ theslope/pages/mytickets.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>01.10.2025 </h1>
+  <h2>Menu - tatziki</h2>
+  <h3>Tilmeldte fra S31</h3>
+  <list-disc>
+    <li> 😋Agata </li>
+    <li> 👶🏻Gæst (barn)</li>
+  </list-disc>
+</template>
