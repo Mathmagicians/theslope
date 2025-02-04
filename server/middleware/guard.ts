0e8a5d63dc9754fa83a2c1ac30cb75ef7cf3099e
themathmagician <agata@mathmagicians.dk>
2025-02-04 10:19:22 +0100

    Guard protection works - a little to well

--- server/middleware/guard.ts
+++ server/middleware/guard.ts
@@ -1,13 +1,13 @@
 import {defineEventHandler} from "h3"
 
 // list of endpoints that we want dont need to protect
-const unprotectedRoutes = ["/api/auth", "/api/calendar/feed"]
+const unprotectedRoutes = ["/api/auth", "/api/calendar/feed", "/login", "/api/_auth", "/api/_nuxt_icon"]
 export default defineEventHandler( async(event) => {
     const url = getRequestURL(event)
     const {pathname} = new URL(url)
     // check if the pathname is in the list of protected routes
-    const shouldBeProtected = pathname.startsWith("api") && !unprotectedRoutes.find((r) => (r===pathname))
-    if (!shouldBeProtected) {
+    const shouldNotBeProtected = pathname === "/" || unprotectedRoutes.find((r) => (pathname.startsWith(r)))
+    if (shouldNotBeProtected) {
         console.log("🔒 > GUARD > Unprotected route: ", pathname)
     } else {
         console.log("🔒 > GUARD > Protected route: ", pathname)
